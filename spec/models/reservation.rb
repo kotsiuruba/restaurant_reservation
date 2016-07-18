f61207a56e133c78064022e019f69f57039a4856
Kotsiuruba Ruslan <ruslan.kotsuruba@gmail.com>
2016-07-18 21:51:55 +0300

    complete tests for reservation

--- spec/models/reservation.rb
+++ spec/models/reservation.rb
@@ -2,57 +2,149 @@ require 'rails_helper'
 
 RSpec.describe Reservation, :type => :model do
 
-  subject { described_class.new }
+  let (:restaurant) {
+    Restaurant.create(
+      :title => "Some restaurant",
+      :open_at => "10:00",
+      :close_at => "20:00"
+    )
+  }
+
+  let (:table) {
+    Table.create(:restaurant => restaurant, :number => 1)
+  }
+
+  subject {
+    described_class.new(
+      :table => table,
+      :start_at => "18.07.2016 12:00",
+      :end_at => "18.07.2016 13:00"
+    )
+  }
 
   it "is valid with valid attributes" do
-    subject.table = Table.first
-    subject.start_at = DateTime.parse "18.07.2016 12:00"
-    subject.end_at = DateTime.parse "18.07.2016 14:00"
     expect(subject).to be_valid
   end
 
   it "is not valid without table" do
-    subject.start_at = DateTime.parse "18.07.2016 12:00"
-    subject.end_at = DateTime.parse "18.07.2016 13:00"
+    subject.table_id = nil
     expect(subject).to_not be_valid
   end
 
   it "is not valid without start_at" do
-    subject.table = Table.first
-    subject.end_at = DateTime.parse "18.07.2016 14:00"
+    subject.start_at = nil
     expect(subject).to_not be_valid
   end
 
   it "is not valid without end_at" do
-    subject.table = Table.first
-    subject.start_at = DateTime.parse "18.07.2016 12:00"
+    subject.end_at = nil
     expect(subject).to_not be_valid
   end
 
   it "is not valid with incorect date interval" do
-    subject.table_id = Table.first.id
-    subject.start_at = DateTime.parse "18.07.2016 12:00"
-    subject.end_at = DateTime.parse "18.07.2016 11:00"
+    subject.start_at = "18.07.2016 12:00"
+    subject.end_at = "18.07.2016 11:00"
     expect(subject).to_not be_valid
   end
 
-  it "is not valid when not matching restaurant time work" do
-    subject.table = Table.first
-    subject.start_at = DateTime.parse(subject.restaurant.open_at) - 1.minute
-    subject.end_at = DateTime.parse(subject.restaurant.open_at) + 1.hour
+  it "is valid when matching rest. time work when open_at<close_at" do
+    subject.restaurant.open_at = "10:00"
+    subject.restaurant.close_at = "20:00"
+    subject.start_at = "18.07.2016 12:00"
+    subject.end_at = "18.07.2016 20:00"
+    expect(subject).to be_valid
+  end
+
+  it "is valid when matching rest. time work when open_at=close_at" do
+    subject.restaurant.open_at = "00:00"
+    subject.restaurant.close_at = "00:00"
+    subject.start_at = "18.07.2016 12:00"
+    subject.end_at = "21.07.2016 20:00"
+    expect(subject).to be_valid
+  end
+
+  it "is valid when matching rest. time work when open_at>close_at" do
+    subject.restaurant.open_at = "20:00"
+    subject.restaurant.close_at = "10:00"
+    subject.start_at = "18.07.2016 05:00"
+    subject.end_at = "18.07.2016 07:00"
+    expect(subject).to be_valid
+  end
+
+  it "is not valid when not matching rest. time work when open_at<close_at" do
+    subject.restaurant.open_at = "10:00"
+    subject.restaurant.close_at = "20:00"
+    subject.start_at = "18.07.2016 01:00"
+    subject.end_at = "18.07.2016 11:00"
     expect(subject).to_not be_valid
   end
 
-  it "is not valid when overlaps another reservation" do
-    subject.table = Table.first
-    another_reservation = Reservation.where(:table_id => subject.table_id).first
-    if !another_reservation.nil?
-      subject.start_at = another_reservation.start_at
-      subject.end_at = another_reservation.end_at
-      expect(subject).to_not be_valid
-    else
-      true
-    end
+  it "is not valid when not matching rest. time work when open_at>close_at" do
+    subject.restaurant.open_at = "20:00"
+    subject.restaurant.close_at = "10:00"
+    subject.start_at = "18.07.2016 01:00"
+    subject.end_at = "19.07.2016 11:00"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid when not matching rest. time work when open_at!=close_at and reservation>1day" do
+    subject.restaurant.open_at = "20:00"
+    subject.restaurant.close_at = "10:00"
+    subject.start_at = "18.07.2016 21:00"
+    subject.end_at = "21.07.2016 09:00"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid when overlaps another reservation: is in another" do
+    another_reservation = Reservation.create(
+      :table_id => subject.table_id,
+      :start_at => "18.07.2016 12:00",
+      :end_at => "18.07.2016 13:00"
+    )
+
+    subject.start_at = "18.07.2016 12:20"
+    subject.end_at = "18.07.2016 13:00"
+    expect(subject).to_not be_valid
+
+  end
+
+  it "is not valid when overlaps another reservation: full cover another" do
+    another_reservation = Reservation.create(
+      :table_id => subject.table_id,
+      :start_at => "18.07.2016 12:00",
+      :end_at => "18.07.2016 13:00"
+    )
+
+    subject.start_at = "18.07.2016 11:59"
+    subject.end_at = "18.07.2016 13:10"
+    expect(subject).to_not be_valid
+
+  end
+
+  it "is not valid when overlaps another reservation: intersects another" do
+    another_reservation = Reservation.create(
+      :table_id => subject.table_id,
+      :start_at => "18.07.2016 12:00",
+      :end_at => "18.07.2016 13:00"
+    )
+
+    subject.start_at = "18.07.2016 11:59"
+    subject.end_at = "18.07.2016 12:10"
+    expect(subject).to_not be_valid
+
+  end
+
+  it "is when not overlaps another reservation" do
+    another_reservation = Reservation.create(
+      :table_id => subject.table_id,
+      :start_at => "18.07.2016 12:00",
+      :end_at => "18.07.2016 13:00"
+    )
+
+    subject.start_at = "18.07.2016 11:59"
+    subject.end_at = "18.07.2016 12:00"
+    expect(subject).to be_valid
+
   end
 
 end
