74504147794cf334feb4a4d52aea4e07e77d5f21
Kotsiuruba Ruslan <ruslan.kotsuruba@gmail.com>
2016-07-18 22:25:08 +0300

    add validations to tables

--- app/models/table.rb
+++ app/models/table.rb
@@ -3,4 +3,7 @@ class Table < ActiveRecord::Base
   belongs_to :restaurant
   has_many :reservations
 
+  validates :number, :presence =>  true
+  validates :number, uniqueness: { scope: :restaurant_id }
+
 end
